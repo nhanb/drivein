35a4974e5107636db698c04843b88405b7c253f1
Bùi Thành Nhân <hi@imnhan.com>
2019-10-24 23:52:47 +0700

    pick any free port

--- server.py
+++ server.py
@@ -3,8 +3,14 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import parse_qs, urlparse
 
 
-def wait_for_authorization_code(port=8000):
-    google_auth_response = None
+def wait_for_authorization_code():
+    """
+    Returns a generator that yields twice:
+        - first, the sucessfully bound port - use this to construct `redirect_uri`
+        - second, the actual authorization code
+
+    https://developers.google.com/identity/protocols/OAuth2InstalledApp#step-2:-send-a-request-to-googles-oauth-2.0-server
+    """
 
     # Prepare a quick http server that serves one GET request
     class OneTimeHandler(BaseHTTPRequestHandler):
@@ -16,16 +22,16 @@ def wait_for_authorization_code(port=8000):
             )
             self.server.google_auth_response = self.path
 
-    with socketserver.TCPServer(("", port), OneTimeHandler) as server:
+    with socketserver.TCPServer(("", 0), OneTimeHandler) as server:
+        port = server.socket.getsockname()[1]
         print("serving at port", port)
         server.handle_request()
+        yield port
         google_auth_response = server.google_auth_response
 
+    # auth response is in the form of GET params, so let's parse them:
     params = parse_qs(urlparse(google_auth_response).query)
     if "code" in params:
-        return params["code"][0]
+        yield params["code"][0]
     else:
         raise Exception(f"Failed to get authorization code: {params['error']}")
-
-
-print(f"got code {wait_for_authorization_code()}")
