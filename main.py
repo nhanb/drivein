90266cce1c9f377ee36fd5fe9c38b3e17fcdf955
nhanb <nhan@nerdyweekly.com>
2019-10-25 14:02:41 +0700

    working on Windows now

--- main.py
+++ main.py
@@ -1,18 +1,100 @@
+import sys
+import webbrowser
+from PyQt5.QtWidgets import (
+    QPushButton,
+    QWidget,
+    QVBoxLayout,
+    QMainWindow,
+    QApplication,
+    QLabel,
+    QDialog,
+)
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtCore import QUrl
 from google import wait_for_authorization_code, build_oauth_url, get_initial_tokens
-from rclone import write_config, mount, kill
+from rclone import write_config, mount, kill, rclone_is_running
 
 
-def google_auth():
-    handler = wait_for_authorization_code()
-    redirect_port = next(handler)
-    oauth_url = build_oauth_url(redirect_port)
-    print(oauth_url)
-    authorization_code = next(handler)
-    tokens = get_initial_tokens(authorization_code, redirect_port)
-    return tokens
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("DriveIn")
+
+        layout = QVBoxLayout()
+        self.init_authorize_button(layout)
+        self.init_rclone_buttons(layout)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        widget.width = 400
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+    def init_authorize_button(self, layout):
+        authorizeBtn = QPushButton("Authorize DriveIn")
+        authorizeBtn.clicked.connect(self.onAuthorizeBtnClicked)
+        self.authorizeBtn = authorizeBtn
+
+        layout.addWidget(QLabel("Step 1:"))
+        layout.addWidget(authorizeBtn)
+
+    def init_rclone_buttons(self, layout):
+        mountBtn = QPushButton("Mount Google Drive")
+        mountBtn.clicked.connect(self.onMountBtnClicked)
+        self.mountBtn = mountBtn
+
+        unmountBtn = QPushButton("Unmount Google Drive")
+        unmountBtn.clicked.connect(self.onUnmountBtnClicked)
+        self.unmountBtn = unmountBtn
+
+        layout.addWidget(QLabel("Step 2:"))
+        layout.addWidget(mountBtn)
+
+        layout.addWidget(QLabel("Step 3 (optional):"))
+        layout.addWidget(unmountBtn)
+
+        if rclone_is_running():
+            mountBtn.setEnabled(False)
+        else:
+            unmountBtn.setEnabled(False)
+
+    def onAuthorizeBtnClicked(self, _):
+        handler = wait_for_authorization_code()
+        redirect_port = next(handler)
+        oauth_url = build_oauth_url(redirect_port)
+
+        QDesktopServices.openUrl(QUrl(oauth_url))
+
+        authorization_code = next(handler)
+        tokens = get_initial_tokens(authorization_code, redirect_port)
+
+        write_config(tokens["refresh_token"])
+        self.authorizeBtn.setDisabled(True)
+        self.authorizeBtn.setText("You're now authorized.")
+
+    def onMountBtnClicked(self, _):
+        self.mount()
+
+    def onUnmountBtnClicked(self, _):
+        self.unmount()
+
+    def unmount(self):
+        kill()
+        self.mountBtn.setEnabled(True)
+        self.unmountBtn.setEnabled(False)
+
+    def mount(self):
+        mount(shell=False)
+        self.mountBtn.setEnabled(False)
+        self.unmountBtn.setEnabled(True)
 
 
-def launch_rclone(refresh_token):
-    write_config(refresh_token)
-    rclone_process = mount()
-    return rclone_process
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
--- rclone.py
+++ rclone.py
@@ -4,7 +4,9 @@ import os
 import signal
 from google import CLIENT_ID, CLIENT_SECRET
 
-REMOTE_NAME = "drivethru"
+# FIXME: mount() and kill() are grossly Windows-specific
+
+REMOTE_NAME = "DriveIn"
 
 
 def produce_config(refresh_token):
@@ -34,19 +36,44 @@ def write_config(refresh_token, filename="rclone.conf"):
         f.write(content)
 
 
-def mount(drive_letter="G") -> subprocess.Popen:
+def mount(drive_letter="G", shell=False) -> subprocess.Popen:
     """
     Spawn a parallel subprocess that mounts the Google Drive remote
     """
-    return subprocess.Popen(
-        f".\\rclone.exe mount {REMOTE_NAME}:/ {drive_letter}: --vfs-cache-mode writes --vfs-read-chunk-size 32M",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        # shell=True so I can send Ctrl+C event:
-        # https://stackoverflow.com/a/26579101
-        shell=True,
-    )
+    if shell:
+        sub = subprocess.Popen(
+            f".\\rclone.exe mount {REMOTE_NAME}:/ {drive_letter}: --vfs-cache-mode writes --vfs-read-chunk-size 32M",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            # shell=True so I can send Ctrl+C event:
+            # https://stackoverflow.com/a/26579101
+            shell=True,
+        )
+    else:
+        sub = subprocess.Popen(
+            [
+                ".\\rclone.exe",
+                "mount",
+                f"{REMOTE_NAME}:/",
+                f"{drive_letter}:",
+                "--vfs-cache-mode",
+                "writes",
+                "--vfs-read-chunk-size",
+                "32M",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+    print(f"Spawned subprocess pid = {sub.pid}")
 
 
-def kill(subpr: subprocess.Popen):
-    os.kill(subpr.pid, signal.CTRL_C_EVENT)
+def kill():
+    # yeeeesh
+    os.system(f"taskkill /im rclone.exe /f")
+
+
+def rclone_is_running():
+    tasklist_result = subprocess.check_output(
+        'tasklist /nh /fi "IMAGENAME eq rclone.exe"'
+    ).decode()
+    return "rclone.exe" in tasklist_result
